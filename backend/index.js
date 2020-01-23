c1528d69deefc19e32e383ece3dc05d1f36306e1
briankramer <bkramer2012@gmail.com>
2020-01-23 10:19:45 -0500

    Try a different format for travis yml access key

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ const pgClient = new Pool({
     password: keys.pgPassword,
     port: keys.pgPort
 })
+
 pgClient.on("error", () => console.log("Lost PG connection"))
 
 pgClient
@@ -30,15 +31,6 @@ pgClient
         "intensity VARCHAR(10)" +
         ")")
     .catch(err => console.log(err))
-
-// // Redis Client Setup
-// const redis = require("redis")
-// const redisClient = redis.createClient({
-//     host: keys.redisHost,
-//     port: keys.redisPort,
-//     retry_strategy: () => 1000
-// })
-// const redisPublisher = redisClient.duplicate()
 
 // Express route handlers
 
@@ -53,26 +45,6 @@ app.get("/practice/all", async (req, res) => {
     const values = await pgClient.query("SELECT * FROM practice")
     res.send(values.rows)
 })
-
-// app.get("/values/current", async (req, res) => {
-//     redisClient.hgetall("values", (err, values) => {
-//         res.send(values)
-//     })
-// })
-//
-// app.post("/values", async (req, res) => {
-//     const index = req.body.index
-//
-//     if (parseInt(index) > 40) {
-//         return res.status(422).send("Index too high")
-//     }
-//
-//     redisClient.hset("values", index, "Nothing yet!")
-//     redisPublisher.publish("insert", index)
-//     pgClient.query("INSERT INTO values(number) VALUES($1)", [index])
-//
-//     res.send({ working: true })
-// })
 
 app.listen(5000, err => {
     console.log("Listening")
